dba8b64cf2902aad0151d4c5934266c681e4aa3a
m-petersen <marvinpetersen94@gmail.com>
2021-07-24 01:26:43 +0200

    [DATALAD] Recorded changes

--- heudiconv_firstpass.sh
+++ heudiconv_firstpass.sh
@@ -8,7 +8,12 @@ module switch singularity singularity/3.5.2-overlayfix
 
 # export project env variables
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source $SCRIPT_DIR/.projectrc
+export PROJ_DIR=$(realpath $SCRIPT_DIR/..) # project root; should be 1 level above code
+export CODE_DIR=$PROJ_DIR/code
+export ENV_DIR=$PROJ_DIR/envs
+export DATA_DIR=$PROJ_DIR/data
+export DCM_DIR=$PROJ_DIR/data/dicoms
+export BIDS_DIR=$PROJ_DIR/data/raw_bids
 export TMPDIR=$WORK/tmp
 
 # Define singularity command and srun flags
