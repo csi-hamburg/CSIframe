70d87c286e6770557d523c5f1e71f1d1375aaa26
m-petersen <marvinpetersen94@gmail.com>
2021-07-23 09:19:37 +0200

    Remove clutter

--- heudiconv_firstpass.sh
+++ heudiconv_firstpass.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-#| # heudiconv first pass
+# heudiconv first pass
 
-#| Import cluster modules
+# Import cluster modules
 source /sw/batch/init.sh
 module switch singularity singularity/3.5.2-overlayfix
 
-#| export project env variables
+# export project env variables
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/.projectrc
 export TMPDIR=$WORK/tmp
 
-#| rename DCM subjects if no sub prefix
-#for dir in $(ls $DCM_DIR); do
-#	if [[ "$dir" != "sub-"* ]]; then
-#	mv $DCM_DIR/$dir $DCM_DIR/sub-${dir}
-#	fi
-#done 
-#ls ${DCM_DIR} | sort -R | tail -3
-
-#| Define singularity command and srun flags
-CMD="singularity run --cleanenv --userns -B $BIDS_DIR:/bids -B $DCM_DIR:/dcm -B $TMPDIR:/tmp $ENV_DIR/heudiconv-0.9.0.sif -d /dcm/{subject}/ses-1/* -s $(ls ${DCM_DIR} | sort -R | tail -3) -f convertall -c none -o /bids"
+# Define singularity command and srun flags
+CMD="singularity run --cleanenv --userns \
+    -B $BIDS_DIR:/bids -B $DCM_DIR:/dcm \
+    -B $TMPDIR:/tmp $ENV_DIR/heudiconv-0.9.0.sif \
+    -d /dcm/{subject}/ses-1/* \
+    -s $(ls ${DCM_DIR} | sort -R | tail -3) \
+    -f convertall \
+    -c none \
+    -o /bids"
 
 #| execute command
 $CMD
