00978a639acf2e06de5f11da21972900f8184ba2
m-petersen <marvinpetersen94@gmail.com>
2021-08-03 09:15:51 +0200

    [DATALAD] Recorded changes

--- pipelines_submission.sh
+++ pipelines_submission.sh
@@ -40,7 +40,7 @@ script_path=$CODE_DIR/$script_name
 
 # empirical job config
 if [ $PIPELINE == "bidsify" ];then
-	export SUBJS_PER_NODE=20  # 6
+	export SUBJS_PER_NODE=15  # 6
 	batch_time="04:00:00"
 	partition="std"
 	at_once=
@@ -51,7 +51,7 @@ if [ $PIPELINE == "bidsify" ];then
 		export SESSION=$input
 	fi
 elif [ $PIPELINE == "qsiprep" ];then
-	export SUBJS_PER_NODE=5  #1
+	export SUBJS_PER_NODE=4  #1
 	batch_time="10:00:00"
 	partition="std" # ponder usage of gpu for eddy speed up
 	at_once=
