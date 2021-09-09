993de8f2d0b47326e8bef3bbaa0bc85f178a6176
Felix L. Nägele <f.naegele@uke.de>
2021-09-09 10:37:55 +0200

    add group level analysis

--- pipelines/mriqc/mriqc.sh
+++ pipelines/mriqc/mriqc.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-CMD="
-   singularity run --cleanenv --userns -B $PROJ_DIR -B $TMP_DIR/:/tmp \
-   $ENV_DIR/mriqc-0.16.1 \
-    data/raw_bids data/mriqc participant \
-    -w /tmp \
-    --participant-label $1 \
-    --modalities T1w T2w bold \
-    --no-sub \
-    --mem_gb $MEM_GB \
-    --ica \
-    --float32 \
-    --nprocs $SLURM_CPUS_PER_TASK"
-$CMD
+if [ $MRIQC_LEVEL == "participant" ]; then
+    CMD="
+    singularity run --cleanenv --userns -B $PROJ_DIR -B $TMP_DIR/:/tmp \
+    $ENV_DIR/mriqc-0.16.1 \
+        data/raw_bids data/mriqc participant \
+        -w /tmp \
+        --participant-label $1 \
+        --modalities T1w T2w bold \
+        --no-sub \
+        --mem_gb $MEM_GB \
+        --ica \
+        --float32 \
+        --nprocs $SLURM_CPUS_PER_TASK"
+    $CMD
+
+elif [ $MRIQC_LEVEL == "group" ]; then
+    CMD="
+    singularity run --cleanenv --userns -B $PROJ_DIR -B $TMP_DIR/:/tmp \
+    $ENV_DIR/mriqc-0.16.1 \
+        data/raw_bids data/mriqc group \
+        -w /tmp \
+        --modalities T1w T2w bold \
+        --no-sub \
+        --mem_gb $MEM_GB \
+        --ica \
+        --float32 \
+        --nprocs $SLURM_CPUS_PER_TASK"
+    $CMD
+
+fi
