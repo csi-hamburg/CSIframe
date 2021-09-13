46db74d9ab3d56f312adc808c471b416b8a13a21
m-petersen <marvinpetersen94@gmail.com>
2021-09-13 15:39:22 +0200

    add multiple session support

--- pipelines/bidsify/bidsify.sh
+++ pipelines/bidsify/bidsify.sh
@@ -9,26 +9,52 @@ fi
 
 # Run heudiconv for dcm2nii and bidsification of its outputs
 # heudiconv_heuristic.py is dataset specific
-CMD="
-   heudiconv \
-   -d /dcm/{subject}/ses-{session}.tar.gz\
-   --subjects $1 \
-   --ses $SESSION \
-   --bids notop \
-   -f /code/pipelines/bidsify/$HEURISTIC\
-   -c dcm2niix \
-   --minmeta \
-   --overwrite\
-   --grouping accession_number \
-   -o /bids"
-singularity run --cleanenv --userns \
---home $PWD \
--B $CODE_DIR:/code \
--B $BIDS_DIR:/bids \
--B $DCM_DIR:/dcm \
--B $TMP_DIR:/tmp \
-$ENV_DIR/heudiconv-0.9.0 \
-$CMD
+if [ $SESSION==all ];then
+   export SESSIONS=$(ls $DCM_DIR/$1/ses-* -d | xargs -n 1 basename)
+   for session in $SESSIONS;do
+      CMD="
+      heudiconv \
+      -d /dcm/{subject}/ses-{session}.tar.gz\
+      --subjects $1 \
+      --ses $SESSION \
+      --bids notop \
+      -f /code/pipelines/bidsify/$HEURISTIC\
+      -c dcm2niix \
+      --minmeta \
+      --overwrite\
+      --grouping accession_number \
+      -o /bids"
+      singularity run --cleanenv --userns \
+      --home $PWD \
+      -B $CODE_DIR:/code \
+      -B $BIDS_DIR:/bids \
+      -B $DCM_DIR:/dcm \
+      -B $TMP_DIR:/tmp \
+      $ENV_DIR/heudiconv-0.9.0 \
+      $CMD
+   done
+else
+   CMD="
+      heudiconv \
+      -d /dcm/{subject}/ses-{session}.tar.gz\
+      --subjects $1 \
+      --ses $SESSION \
+      --bids notop \
+      -f /code/pipelines/bidsify/$HEURISTIC\
+      -c dcm2niix \
+      --minmeta \
+      --overwrite\
+      --grouping accession_number \
+      -o /bids"
+   singularity run --cleanenv --userns \
+   --home $PWD \
+   -B $CODE_DIR:/code \
+   -B $BIDS_DIR:/bids \
+   -B $DCM_DIR:/dcm \
+   -B $TMP_DIR:/tmp \
+   $ENV_DIR/heudiconv-0.9.0 \
+   $CMD
+fi
 
 chmod 770 -R $BIDS_DIR/sub-${1}
 
@@ -37,21 +63,27 @@ singularity="singularity run --cleanenv --userns -B . -B $TMP_DIR/:/tmp"
 
 T1=$BIDS_DIR/sub-${1}/ses-${SESSION}/anat/sub-${1}_ses-${SESSION}_T1w.nii.gz
 CMD_T1="pydeface $T1 --outfile $T1 --force --verbose"
-$singularity \
-$ENV_DIR/pydeface-2.0.0 \
-/bin/bash -c "$CMD_T1"
+if [ -f $T1 ];then
+   $singularity \
+   $ENV_DIR/pydeface-2.0.0 \
+   /bin/bash -c "$CMD_T1"
+fi
 
 T2=$BIDS_DIR/sub-${1}/ses-${SESSION}/anat/sub-${1}_ses-${SESSION}_T2w.nii.gz
 CMD_T2="pydeface $T2 --outfile $T2 --force --verbose"
-$singularity \
-$ENV_DIR/pydeface-2.0.0 \
-$CMD_T2
+if [ -f $T2 ];then
+   $singularity \
+   $ENV_DIR/pydeface-2.0.0 \
+   $CMD_T2
+fi
 
 FLAIR=$BIDS_DIR/sub-${1}/ses-${SESSION}/anat/sub-${1}_ses-${SESSION}_FLAIR.nii.gz
 CMD_FLAIR="pydeface $FLAIR --outfile $FLAIR --force --verbose"
-$singularity \
-$ENV_DIR/pydeface-2.0.0 \
-$CMD_FLAIR
+if [ -f $FLAIR ];then
+   $singularity \
+   $ENV_DIR/pydeface-2.0.0 \
+   $CMD_FLAIR
+fi
 
 # Remove problematic metadata from json sidecars
 CMD="python $CODE_DIR/pipelines/$PIPELINE/handle_metadata.py $1"
--- pipelines_submission.sh
+++ pipelines_submission.sh
@@ -46,7 +46,7 @@ if [ $PIPELINE == "bidsify" ];then
 	subj_array=(${input_subject_array[@]-$(ls $DCM_DIR/* -d -1 | grep -v -e code -e sourcedata | xargs -n 1 basename)}) # subjects in data/dicoms
 
 	echo "What heuristic do you want to apply?"
-	echo "Choose from" $(ls $CODE_DIR/pipelines/bidsify/heudiconv_*)
+	echo "Choose from" $(ls $CODE_DIR/pipelines/bidsify/heudiconv_*.py | xargs -n 1 basename)
 	read HEURISTIC; export HEURISTIC
 
 elif [ $PIPELINE == "qsiprep" ];then
@@ -72,6 +72,21 @@ elif [ $PIPELINE == "smriprep" ];then
 	batch_time="23:00:00"
 	partition="std"
 	at_once=
+
+	echo "Do you want to use specific templates? (y/n)"
+	echo "Enter 'n' to keep defaults (fsnative fsaverage MNI152NLin6Asym anat)"
+	read SPECIFIC_TEMP; export SPECIFIC_TEMP
+
+	if [ $SPECIFIC_TEMP == y ];then
+		echo "Choose from tested adult templates (fsnative fsaverage MNI152NLin6Asym anat)"
+		echo "or infant templates (MNIPediatricAsym:cohort-1:res-native)"
+		read OUTPUT_SPACES; export OUTPUT_SPACES
+	elif [ $SPECIFIC_TEMP == n ];then
+		# Keep templates
+		export OUTPUT_SPACES="fsnative fsaverage MNI152NLin6Asym anat"
+	else
+		echo "Please answer with 'y' or 'n'"
+	fi
 
 elif [ $PIPELINE == "mriqc" ];then
 
@@ -99,6 +114,21 @@ elif [ $PIPELINE == "fmriprep" ];then
 	batch_time="15:00:00"
 	partition="std"
 	at_once=
+
+	echo "Do you want to use specific templates? (y/n)"
+	echo "Do not enter anything to keep defaults (fsnative fsaverage MNI152NLin6Asym)"
+	read SPECIFIC_TEMP; export SPECIFIC_TEMP
+
+	if [ $SPECIFIC_TEMP == y ];then
+		echo "Choose from tested adult templates (fsnative fsaverage MNI152NLin6Asym anat)"
+		echo "or infant templates (MNIPediatricAsym:cohort-1:res-1)"
+		read OUTPUT_SPACES; export OUTPUT_SPACES
+	elif [ $SPECIFIC_TEMP == n ];then
+		# Keep templates
+		export OUTPUT_SPACES="fsnative fsaverage MNI152NLin6Asym"
+	else
+		echo "Please answer with 'y' or 'n'"
+	fi
 
 elif [ $PIPELINE == "xcpengine" ];then
 	export SUBJS_PER_NODE=16
