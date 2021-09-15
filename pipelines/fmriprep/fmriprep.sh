968913c4f440060c30464e4c3d9ac6f356e59926
m-petersen <marvinpetersen94@gmail.com>
2021-09-15 09:40:25 +0200

    new container

--- pipelines/fmriprep/fmriprep.sh
+++ pipelines/fmriprep/fmriprep.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 CMD="
-   singularity run --cleanenv --userns -B $PROJ_DIR -B $TMP_DIR/:/tmp \
-   $ENV_DIR/fmriprep-20.2.3 \
+   singularity run --cleanenv --userns -B $PROJ_DIR -B /work/fatx405/tmp/:/tmp \
+   $ENV_DIR/fmriprep-21.0.0rc0 \
    data/raw_bids data participant \
    -w /tmp \
    --participant-label $1 \
-   --output-spaces fsnative fsaverage MNI152NLin6Asym \
+   --output-spaces $OUTPUT_SPACES \
    --nthreads $SLURM_CPUS_PER_TASK \
    --omp-nthreads $OMP_NTHREADS \
    --mem-mb $MEM_MB \
+   --bids-database-dir $CODE_DIR/bids_database \
    --stop-on-first-crash \
    --ignore t2w \
-   --skip-bids-validation \
    --fs-subjects-dir data/freesurfer \
    --use-aroma \
    --random-seed 12345 \
    --use-syn-sdc \
+   --cifti-output 91k \
+   --notrack \
+   --skip_bids_validation \
    --fs-license-file envs/freesurfer_license.txt"
 $CMD
