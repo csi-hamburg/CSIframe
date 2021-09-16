0240a99edf9aa169061ef93d309add4de7c820f8
m-petersen <marvinpetersen94@gmail.com>
2021-09-16 16:56:43 +0200

    debug obseg

--- pipelines/obseg/obseg.sh
+++ pipelines/obseg/obseg.sh
@@ -20,8 +20,8 @@ singularity="singularity exec --cleanenv --userns -B $PROJ_DIR -B $TMP_DIR:/tmp"
 T2=data/raw_bids/$1/ses-${SESSION}/anat/${1}_ses-${SESSION}_T2w.nii.gz
 
 # Define commands
-CMD_OBSEG="python3 /olf-bulb-segmentation/run_pipeline.py" #-in $T2 -out $OUT_DIR -sid $1 -ncuda"
-#singularity exec olfsegnet_cpu-latest/ python3 /olf-bulb-segmentation/run_pipeline.py
+CMD_OBSEG="python3 /olf-bulb-segmentation/run_pipeline.py -in $T2 -out $OUT_DIR -sid $1 -ncuda"
+
 # Execute 
 $singularity \
 $OBSEG_CONTAINER \
