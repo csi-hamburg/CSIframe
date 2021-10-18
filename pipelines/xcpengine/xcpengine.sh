ccba86de64b3d14ab16d907f9ec20b0029457d93
m-petersen <marvinpetersen94@gmail.com>
2021-10-18 19:16:43 +0200

    add xcpengine

--- pipelines/xcpengine/xcpengine.sh
+++ pipelines/xcpengine/xcpengine.sh
@@ -3,25 +3,41 @@
 # Set specific environment for pipeline
 XCPENGINE_VERSION=xcpengine-1.2.3
 
-OUT_DIR=data/xcpengine/$1/ses-${SESSION}/func/
-[ ! -d $OUT_DIR ] && mkdir -p $OUT_DIR
 
 # Create cohort file
 #########################
-BOLD_IMG=$1/$SESSION/func/${1}_ses-${SESSION}_task-rest_space-MNI152NLin6Asym_desc-preproc_bold.nii.gz
 COHORT_FILE=$TMP_DIR/${1}_ses-${SESSION}_cohort_file.txt
-REFERENCE_DIR=data/fmriprep
-echo "$1 $T1_IN_FLAIR $FLAIR_TO_MNI_WARP" > $COHORT_FILE
 
+if [ $MODIFIER == struct ];then
+
+   BOLD_IMG=$1/$SESSION/func/${1}_ses-${SESSION}_task-rest_space-MNI152NLin6Asym_desc-preproc_bold.nii.gz
+   REFERENCE_DIR=data/fmriprep
+   echo "$1 $BOLD_IMG" > $COHORT_FILE
+
+   OUT_DIR=data/xcpengine/
+   [ ! -d $OUT_DIR ] && mkdir -p $OUT_DIR
+
+elif [ $MODIFIER == fc ];then
+
+   BOLD_IMG=$1/$SESSION/func/${1}_ses-${SESSION}_task-rest_space-MNI152NLin6Asym_desc-preproc_bold.nii.gz
+   REFERENCE_DIR=data/fmriprep
+   echo "id0, img" > $COHORT_FILE
+   echo "$1 $BOLD_IMG" > $COHORT_FILE
+
+   OUT_DIR=data/xcpengine/
+   [ ! -d $OUT_DIR ] && mkdir -p $OUT_DIR
+
+fi
 
 CMD="
    singularity run --cleanenv --userns -B $PROJ_DIR -B $TMP_DIR/:/tmp \
    $ENV_DIR/$XCPENGINE_VERSION \
-   -d code/pipelines/xpengine/$DESIGN \
+   -d code/pipelines/xcpengine/$DESIGN \
    -c /tmp/${1}_ses-${SESSION}_cohort_file.txt \
    -r $REFERENCE_DIR \
-   -o $OUT_DIR"
-[ ! -z $MODIFIER ] && CMD="${CMD} $MODIFIED"
+   -i /tmp \
+   -o $OUT_DIR \
+   -t 1"
 $CMD
 
 
