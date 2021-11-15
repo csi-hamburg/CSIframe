f20c6a2b7ed2838198a4173d71c6f4bccc8cfec9
Felix L. Nägele <f.naegele@uke.de>
2021-11-15 14:17:00 +0100

    pipeline submission adaption for bidsify asl

--- pipelines_submission.sh
+++ pipelines_submission.sh
@@ -68,6 +68,11 @@ if [ $PIPELINE == "bidsify" ];then
 	subj_array=(${input_subject_array[@]-$(ls $DCM_DIR/* -d -1 | grep -v -e code -e sourcedata -e README | xargs -n 1 basename)}) # subjects in data/dicoms
 	subj_array_length=${#subj_array[@]}
 
+	echo "◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼"		
+	echo "Which bidsify pipeline do you want to perform? Leave empty for core or type 'asl'."
+	read BIDSIFY_PIPE; export BIDSIFY_PIPE
+	export PIPELINE_SUFFIX=_${BIDSIFY_PIPE}
+
 	echo "◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼️◼"
 	echo "Which heuristic do you want to apply?"
 	echo "Choose from" $(ls $CODE_DIR/pipelines/bidsify/heudiconv_*.py | xargs -n 1 basename)
