f6d64ebe23e8426b15ec5a248acfc7106a8f7e95
Felix L. Nägele <f.naegele@uke.de>
2021-11-30 16:28:46 +0100

    aslprep set up

--- pipelines/aslprep/aslprep.sh
+++ pipelines/aslprep/aslprep.sh
@@ -1,40 +1,78 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Set specific environment for pipeline
-export MRTRIX_TMPFILE_DIR=$TMP_DIR
+######################################################################
+# ASL analysis based on https://aslprep.readthedocs.io/en/latest/    #
+#                                                                    #
+# Pipeline specific dependencies:                                    #
+#   [pipelines which need to be run first]                           #
+#       - bidsify                                                    #
+#   [container]                                                      #
+#       - aslprep-0.2.7.sif                                          #
+######################################################################
 
-# To make I/O more efficient read/write outputs from/to scratch #
-TMP_IN=$TMP_DIR/input
-TMP_OUT=$TMP_DIR/output
-[ ! -d $TMP_IN ] && mkdir -p $TMP_IN && cp -rf $BIDS_DIR/$1 $BIDS_DIR/dataset_description.json $TMP_IN 
-[ ! -d $TMP_OUT ] && mkdir -p $TMP_OUT/freesurfer
-[ ! -f $DATA_DIR/freesurfer/$1/stats/aseg.stats ] && rm -rf $DATA_DIR/freesurfer/$1 || cp -rf $DATA_DIR/freesurfer/$1 $TMP_OUT/freesurfer
+# Get verbose outputs
+set -x
+
+# Define subject specific temporary directory on $SCRATCH_DIR
+export TMP_DIR=$SCRATCH_DIR/$1/tmp/;   [ ! -d $TMP_DIR ] && mkdir -p $TMP_DIR
+TMP_IN=$TMP_DIR/input;                 [ ! -d $TMP_IN ] && mkdir -p $TMP_IN
+TMP_OUT=$TMP_DIR/output;               [ ! -d $TMP_OUT ] && mkdir -p $TMP_OUT
+
+###################################################################################################################
+
+# Define environment
+####################
+
+container_aslprep=aslprep-0.2.7
+singularity_aslprep="singularity run --cleanenv --userns \
+    -B $PROJ_DIR \
+    -B $(readlink -f $ENV_DIR) \
+    -B $TMP_DIR/:/tmp \
+    -B $TMP_IN:/tmp_in \
+    -B $TMP_OUT:/tmp_out \
+    $ENV_DIR/$container_aslprep" 
+
+# To make I/O more efficient read/write outputs from/to $SCRATCH
+[ -d $TMP_IN ] && cp -rf $BIDS_DIR/$1 $BIDS_DIR/dataset_description.json $TMP_IN 
+[ -d $TMP_OUT ] && mkdir -p $TMP_OUT/aslprep
+
+###########
+# ASLprep #
+###########
+
+# Define command
+################
 
 CMD="
-   singularity run --cleanenv --userns -B $PROJ_DIR -B $TMP_DIR/:/tmp \
-   $ENV_DIR/aslprep-0.2.7 \   # '-B' zuvor gesetzt, da Variable definiert wird?
-   data/raw_bids $DATA_DIR/aslprep {participant} \
+   $singularity_aslprep \
+   /tmp_in /tmp_out participant \
    --participant-label $1 \
    --work-dir /tmp \
-   --clean-workdir \
+   --fs-license-file envs/freesurfer_license.txt
    --skip-bids-validation \
-   --use-syn-sdc \ # EXPERIMENTAL: Use fieldmap-free distortion correction; mit Vorteil behaftet? (Marvin). Ggf. --force-syn zusätzlich zu nutzen
-   --output-space func T1w MNI152NLin2009cAsym \
    --nthreads $SLURM_CPUS_PER_TASK \
    --omp-nthreads $OMP_NTHREADS \
    --mem_mb $MEM_MB \
+   --ignore fieldmaps slicetiming sbref \
+   --output-spaces asl T1w MNI152NLin2009cAsym \
+   --asl2t1w-init register \
+   --asl2tw-dof 6 \
+   --m0_scale 0.1 \
+   --scorescrub \
+   --basil \
+   --use-syn-sdc \
    --skull-strip-template OASIS \
    --skull-strip-fixed-seed 42
+   --skull-strip-t1w force
    --stop-on-first-crash \
-   --notrack \
-   --output-resolution $OUTPUT_RESOLUTION \
-   --fs-license-file envs/freesurfer_license.txt"
+   --notrack"
 
-#[ ! -z $RECON ] && CMD="${CMD} --recon_input data/qsiprep/$1 --recon_spec $RECON"
-[# ! -z $MODIFIER ] && CMD="${CMD} $MODIFIED"
-$CMD
+# Execute command
+#################
 
-cp -ruvf $TMP_OUT/freesurfer $DATA_DIR
-cp -ruvf $TMP_OUT/sub-* $DATA_DIR/aslprep
-cp -ruvf $TMP_OUT/d* $DATA_DIR/aslprep
-cp -ruvf $TMP_OUT/logs $DATA_DIR/aslprep
+eval $CMD
+
+# Copy outputs to $DATA_DIR
+###########################
+
+cp -ruvf $TMP_OUT/aslprep $DATA_DIR/aslprep
